8b5b329d74c9f38f80e563d867b83fb937138f05
Eric Mueller <eric.mueller1024@gmail.com>
2015-07-20 02:09:02 -0700

    added a kernel-style BUG() macro to util.h that bails with a nice message if things go bad. (i.e., in the defaault of a switch statement that should never be hit)

--- src/util.h
+++ src/util.h
@@ -2,10 +2,19 @@
 #define PNG_UTIL_H
 
 #include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 /* random shit goes here */
 
 #define container_of(__ptr, __type, __member)   \
         ((__type *)((char *)(__ptr) - offsetof(__type, __member)))
+
+
+#define BUG()                                                           \
+        do {                                                            \
+                fprintf(stderr, "BUG! %s:%d %s", __FILE__, __LINE__, __func__); \
+                exit(1);                                                \
+        } while (0);                                                    \
 
 #endif /* PNG_UTIL_H */
